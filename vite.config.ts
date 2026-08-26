d64a56a8a6bccfc109ec36a88e05a60cb1079a24
Nazmus Sakib <nazmussakib247@users.noreply.github.com>
2026-08-26 12:34:19 +0600

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig({
+  server: {
+    host: "::",
+    port: 8080,
+    hmr: { overlay: false },
+    proxy: {
+      "/api": "http://localhost:3000",
+    },
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
